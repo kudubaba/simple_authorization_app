4036f47675625a5580f190fdd44203ffe6760481
kudubaba <kudubaba@gmail.com>
2018-03-07 10:57:42 -0500

    Added sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+
+
+def new
+  @user = User.new
+end
+
+def create
+  @user = User.find_by(username: session_params[:username])
+
+  if @user && @user.authenticate(session_params[:password])
+      session[:user_id] = @user.id
+      redirect_to root
+  else
+    flash[:errors] = ["Invalid Username/Password Combination"]
+    render :new
+  end
+end
+
+def destroy
+  session[:user_id] = nil
+  redirect_to login_path
+end
+
+  private
+
+def session_params
+  params.require(:user).permit(:username, :password)
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :edit, :update]
+  get   '/login'  =>  'sessions#new',     as: 'login'
+  post  '/login'  =>   'sessions#create'
+  get   '/logout' =>  'sessions#destroy', as: 'logout'
 end
